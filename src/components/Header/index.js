5fd7970ca91d1acaba1ee02720b4ae64cfda8937
Nareshmagatham <nareshmagatham@gmail.com>
2024-02-20 13:07:23 +0000

    Solved

---
+++ src/components/Header/index.js
@@ -0,0 +1,98 @@
+import './index.css'
+import Cookies from 'js-cookie'
+import {Link, withRouter} from 'react-router-dom'
+
+const Header = props => {
+  const onLogoutClicked = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <div className="nav-bar-mobile-logo-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+
+          <button
+            type="button"
+            className="nav-mobile-btn"
+            onClick={onLogoutClicked}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+              alt="nav logout"
+              className="nav-bar-img"
+            />
+          </button>
+        </div>
+
+        <div className="nav-content nav-bar-large-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+          <ul className="nav-menu">
+            <li className="nav-menu-item">
+              <Link to="/"> Home</Link>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link to="/products">Products</Link>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+          <button
+            type="button"
+            className="logout-desktop-btn"
+            onClick={onLogoutClicked}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="nav-menu-mobile">
+        <ul className="nav-menu-list-mobile">
+          <li className="nav-menu-item-mobile">
+            <Link to="/">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+                alt="nav home"
+                className="nav-bar-img"
+              />
+            </Link>
+          </li>
+
+          <li className="nav-menu-item-mobile">
+            <Link to="/products">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+                alt="nav products"
+                className="nav-bar-img"
+              />
+            </Link>
+          </li>
+
+          <li className="nav-menu-item-mobile">
+            <Link to="/cart">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+                alt="nav cart"
+                className="nav-bar-img"
+              />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default withRouter(Header)
---
+++ src/components/Products/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
